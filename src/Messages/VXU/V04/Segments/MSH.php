f68f8c304bb9a3f41b97027697b71fe47815310a
R Hunter Wigelsworth <hunter@wigelsworth.io>
2017-03-28 17:06:32 -0400

    comment code and (even though YmdHis should always return 14) add 14 char max to sprintf

--- src/Messages/VXU/V04/Segments/MSH.php
+++ src/Messages/VXU/V04/Segments/MSH.php
@@ -8,11 +8,15 @@ namespace Immunable\Messages\VXU\V04\Segments;
 class MSH extends \Immunable\Messages\Segments\MSH {
 
   public function __construct(): void {
+    // Set type and profile for VXU message
     $this->messageType = 'VXU^V04^VXU_V04';
     $this->messageProfileId = 'Z22^CDCPHINVS';
+    // Set dateTime to right now
     $this->dateTime = new \DateTime();
+    // Set controlId to DateTime plus random number for now
+    // Can be retrieved or set using getControlId() / setControlId() methods
     $this->messageControlId = sprintf(
-      "%s%06d",
+      "%.14s%06d",
       $this->dateTime->format('YmdHis'),
       mt_rand(1, 999999)
     );
